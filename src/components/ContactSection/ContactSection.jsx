47a03f32a89d506c0d8ee67fb6befbcf758ae905
Balausa <myrzabekovabalausa&2gmail.com>
2025-10-04 11:34:35 +0500

    asasas

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -17,32 +17,6 @@ const ContactSection = () => {
                 </div>
 
                 <div className="contact_info">
-                    <div className="info">
-                        <h1>Contact Information</h1>
-                        <div className="info_wrapper">
-                            <div className="info_card">
-                                <img src="" alt="" />
-                                <div className="info_text">
-                                    <h3>Email</h3>
-                                    <p>email@email.com</p>
-                                </div>
-                            </div>
-                            <div className="info_card">
-                                <img src="" alt="" />
-                                <div className="info_text">
-                                    <h3>Phone</h3>
-                                    <p>+7 777 777 77 77</p>
-                                </div>
-                            </div>
-                            <div className="info_card">
-                                <img src="" alt="" />
-                                <div className="info_text">
-                                    <h3>Location</h3>
-                                    <p>Kazakhstan, Almaty</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <form className="contact_form">
                         <h1>Send a Message</h1>
                         <div>
